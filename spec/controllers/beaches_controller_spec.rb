23a84005d3eac29dc0825500d9d422d763b0d300
fathergoose <al@ilseman.com>
2016-05-29 22:13:16 -0500

    to be implimented tests

--- spec/controllers/beaches_controller_spec.rb
+++ spec/controllers/beaches_controller_spec.rb
@@ -7,9 +7,15 @@ describe BeachesController do
       get 'index'
       response.should be_success
     end
+
     it "assigns an instance variable @beaches" do
       get 'index'
       assigns(:beaches).should be_an(Array)
+    end
+
+    it "returns only the latest measurement for each beach" do
+      skip('Only one measurement')
+      skip('Make a mock response like the data api')
     end
   end
 
