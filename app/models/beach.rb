e1046747818c32727b0048d4214585357ab333b6
fathergoose <al@ilseman.com>
2016-06-04 15:27:14 -0500

    note for success

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -4,6 +4,9 @@ class Beach
     # This is a querry example that may work
     # TODO: Incorporate a app token into the request though ENV
     # TODO: Impliment latest_readings
+    # Okay, doing it all wrong, need to use the 'soda-ruby' gem so as not to re-invent the SDK wheel
+    # resp = client.get( "46rk-hgnz", {"$where" => "measurement_timestamp > '2016-06-03T00:00:00' AND measurement_timestamp < '2016-06-04T00:00:00'"})
+    # Using the 'soda-ruby' library, this is a working querry
   end
 
 end
