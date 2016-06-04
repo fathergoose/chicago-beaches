c8608b8b41848cf8309f8f8a413042451e174303
fathergoose <al@ilseman.com>
2016-06-04 15:50:01 -0500

    semi successfull current_readings method

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,6 +1,7 @@
 class BeachesController < ApplicationController
   def index
-    @beaches = Unirest.get('https://data.cityofchicago.org/resource/46rk-hgnz.json').body
+    # @beaches = Unirest.get('https://data.cityofchicago.org/resource/46rk-hgnz.json').body
+    @beaches = Beach.latest_readings
   end
 
   def show
--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,5 +1,6 @@
 class Beach
-  def latest_readings
+  def self.latest_readings
+    dataset_id = '46rk-hgnz'
     # "https://data.cityofchicago.org/resource/46rk-hgnz.json?$where=date between '2015-01-10T12:00:00' and '2015-01-10T14:00:00'"
     # This is a querry example that may work
     # TODO: Incorporate a app token into the request though ENV
@@ -7,6 +8,12 @@ class Beach
     # Okay, doing it all wrong, need to use the 'soda-ruby' gem so as not to re-invent the SDK wheel
     # resp = client.get( "46rk-hgnz", {"$where" => "measurement_timestamp > '2016-06-03T00:00:00' AND measurement_timestamp < '2016-06-04T00:00:00'"})
     # Using the 'soda-ruby' library, this is a working querry
+
+
+    client = SODA::Client.new({ domain: 'data.cityofchicago.org',
+                                app_token: ENV['$CHICAGO_BEACH_APP_TOKEN'] })
+    response = client.get( dataset_id, {"$where" => "measurement_timestamp > '2016-06-03T00:00:00' AND measurement_timestamp < '2016-06-04T00:00:00'"})
+    beaches = response
   end
 
 end
