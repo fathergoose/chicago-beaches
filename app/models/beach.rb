484c1f2478924f958382579c9c21e6011ac8b232
fathergoose <al@ilseman.com>
2016-06-10 22:51:15 -0500

    refactoring

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -7,36 +7,18 @@
     # using the 'soda-ruby' library, this is a working querry
 
 
-    Time::DATE_FORMATS[:soda] = '%Y-%m-%dT%H:%M:%S'
+Time::DATE_FORMATS[:soda] = '%Y-%m-%dT%H:%M:%S'
 
 class Beach
-  attr_accessor :water_temperature
   def self.latest_readings
     dataset_id = '46rk-hgnz'
-
-
-    right_now = Time.now.to_s :soda
-    an_hour_ago = (Time.now - 1.hour).to_s :soda
-    two_hours_ago = (Time.now - 2.hour).to_s :soda
     one_day_ago = (Time.now - 1.day).to_s :soda
-
-    puts 'NOWNOWNOWNOWNOWNOWNOWNOW'
-    puts right_now
-    puts an_hour_ago
-    puts one_day_ago
-
     client = SODA::Client.new({ domain: 'data.cityofchicago.org',
                                 app_token: ENV['$CHICAGO_BEACH_APP_TOKEN'] })
-    response = client.get( dataset_id,
+    readings = client.get( dataset_id,
                           {"$where" => "measurement_timestamp > '#{one_day_ago}'",
                            "$order" => "measurement_timestamp DESC",
                            "$limit" => "3"})
-    response.map { |r| BeachData::WaterQualityAutoSenseReading.new(r) }
+    readings.map { |r| BeachData::WaterQualityAutoSenseReading.new(r) }
   end
-
-
-  def water_temperature_in_fahrenheit
-    water_temperature * 1.8 + 32
-  end
-
 end
