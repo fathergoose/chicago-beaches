ecc323531cfcc58c0aa9e684788a632efde8757d
fathergoose <al@ilseman.com>
2016-06-11 22:18:13 -0500

    annotate the class to say wtf it does

--- app/models/beach_data/water_quality_auto_sense_reading.rb
+++ app/models/beach_data/water_quality_auto_sense_reading.rb
@@ -1,13 +1,26 @@
 module BeachData
   class WaterQualityAutoSenseReading
-    attr_reader(:battery_life,
+    # This class represents a specific reading from the endpoint
+    # at data.cityofchicago.org/resource/46rk-hgnz.json
+    # Quoting from the Beach Water Quality - Automated Sensors
+    # webpage:
+    # "The Chicago Park District maintains sensors in the water at
+    # beaches along Chicago's Lake Michigan lakefront. These sensors
+    # generally capture the indicated measurements hourly while the
+    # sensors are in operation during the summer. During other seasons
+    # and at some other times, information from the sensors may not
+    # be available. See https://data.cityofchicago.org/d/k7hf-8y75 for
+    # a dataset with land-based weather measurements at selected
+    # beaches. The sensor locations are listed at 
+    # https://data.cityofchicago.org/d/g3ip-u8rb."
+    attr_reader :battery_life,
                 :beach_name, 
                 :measurement_timestamp,
                 :transducer_depth,
                 :turbidity,
                 :water_temperature,
                 :wave_height,
-                :wave_period)
+                :wave_period,
 
     def initialize(reading)
       @battery_life = reading.battery_life
@@ -25,6 +38,8 @@ module BeachData
       fahrenheit_float.round(1)
     end
 
+    # Return a descriptive string estimating how long ago
+    # the last measurement was taken. "...ago"
     def human_friendly_time
       seconds_ago = Time.now - @measurement_timestamp
       if seconds_ago < 3600
@@ -41,9 +56,9 @@ module BeachData
 
     private
 
+    # The arrogant Chi_data_api just uses local time with no stamp :/
+    # So we add it in manually with -05:00
     def set_date_time(api_time)
-      # The arrogant Chi_data_api just uses local time with no stamp :/
-      # So we add it in manually as -05:00
       DateTime.strptime("#{api_time}-05:00", '%Y-%m-%dT%H:%M:%S.%L%z')
     end
   end
