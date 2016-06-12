649142f985e1d9e484723e8b8dfd276817e20007
fathergoose <al@ilseman.com>
2016-06-11 21:16:29 -0500

    removed crufty comments

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,14 +1,4 @@
-    # "https://data.cityofchicago.org/resource/46rk-hgnz.json?$where=date between '2015-01-10T12:00:00' and '2015-01-10T14:00:00'"
-    # This is a querry example that may work
-    # TODO: Incorporate a app token into the request though ENV
-    # TODO: Impliment latest_readings
-    # Okay, doing it all wrong, need to use the 'soda-ruby' gem so as not to re-invent the SDK wheel
-    # resp = client.get( "46rk-hgnz", {"$where" => "measurement_timestamp > '2016-06-03T00:00:00' AND measurement_timestamp < '2016-06-04T00:00:00'"})
-    # using the 'soda-ruby' library, this is a working querry
-
-
 Time::DATE_FORMATS[:soda] = '%Y-%m-%dT%H:%M:%S'
-
 class Beach
   def self.latest_readings
     dataset_id = '46rk-hgnz'
