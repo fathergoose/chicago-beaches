cadb7ee2e9dba97d120693baf31ad78e66aa2175
fathergoose <al@ilseman.com>
2016-06-12 22:33:16 -0500

    refactored beach-data specific info into BeachData::

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,17 +1,5 @@
 class Beach
   def self.latest_readings
-    dataset_id = '46rk-hgnz'
-    one_day_ago = (Time.now - 1.day).to_s(:soda)
-    client = SODA::Client.new(
-      domain: 'data.cityofchicago.org',
-      app_token: ENV['$CHICAGO_BEACH_APP_TOKEN'],
-    )
-    readings = client.get(
-      dataset_id,
-      "$where" => "measurement_timestamp > '#{one_day_ago}'",
-      "$order" => "measurement_timestamp DESC",
-      "$limit" => "3",
-    )
-    readings.map { |r| BeachData::WaterQualityAutoSenseReading.new(r) }
+    BeachData::WaterQualityAutoSenseReading.latest_readings
   end
 end
--- app/models/beach_data/water_quality_auto_sense_reading.rb
+++ app/models/beach_data/water_quality_auto_sense_reading.rb
@@ -13,6 +13,28 @@ module BeachData
     # a dataset with land-based weather measurements at selected
     # beaches. The sensor locations are listed at 
     # https://data.cityofchicago.org/d/g3ip-u8rb."
+
+    DATASET_ID = '46rk-hgnz'
+
+
+    def self.latest_readings
+      client = SODA::Client.new(
+        domain: 'data.cityofchicago.org',
+        app_token: ENV['$CHICAGO_BEACH_APP_TOKEN'],
+      )
+
+      one_day_ago = (Time.now - 1.day).to_s(:soda)
+
+      readings = client.get(
+        DATASET_ID,
+        "$where" => "measurement_timestamp > '#{one_day_ago}'",
+        "$order" => "measurement_timestamp DESC",
+        "$limit" => "3",
+      )
+
+      readings.map { |r| BeachData::WaterQualityAutoSenseReading.new(r) }
+    end
+
     attr_reader(
       :battery_life,
       :beach_name,
