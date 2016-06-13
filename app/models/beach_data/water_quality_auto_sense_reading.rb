d6c2c0a405123551e7a68e5247d037d10ea17876
fathergoose <al@ilseman.com>
2016-06-13 17:10:05 -0500

    added an interface for all api readings and refactored

--- app/models/beach_data/water_quality_auto_sense_reading.rb
+++ app/models/beach_data/water_quality_auto_sense_reading.rb
@@ -13,32 +13,38 @@ module BeachData
     # a dataset with land-based weather measurements at selected
     # beaches. The sensor locations are listed at 
     # https://data.cityofchicago.org/d/g3ip-u8rb."
+    
+    class << self
+      def latest_readings
+        client = make_client
+        one_day_ago = (Time.now - 1.day).to_s(:soda)
+        readings = client.get(
+          DATASET_ID,
+          "$where" => "measurement_timestamp > '#{one_day_ago}'",
+          "$order" => "measurement_timestamp DESC",
+          "$limit" => "3",
+        )
+        readings.map { |r| BeachData::WaterQualityAutoSenseReading.new(r) }
+      end
 
-    DATASET_ID = '46rk-hgnz'
+      private
 
+      DATASET_ID = '46rk-hgnz'
 
-    def self.latest_readings
-      client = SODA::Client.new(
-        domain: 'data.cityofchicago.org',
-        app_token: ENV['$CHICAGO_BEACH_APP_TOKEN'],
-      )
-
-      one_day_ago = (Time.now - 1.day).to_s(:soda)
-
-      readings = client.get(
-        DATASET_ID,
-        "$where" => "measurement_timestamp > '#{one_day_ago}'",
-        "$order" => "measurement_timestamp DESC",
-        "$limit" => "3",
-      )
-
-      readings.map { |r| BeachData::WaterQualityAutoSenseReading.new(r) }
+      def make_client
+        SODA::Client.new(
+          domain: 'data.cityofchicago.org',
+          app_token: ENV['$CHICAGO_BEACH_APP_TOKEN'],
+        )
+      end
     end
 
     attr_reader(
       :battery_life,
       :beach_name,
+      :measurement_id,
       :measurement_timestamp,
+      :measurement_timestamp_label,
       :transducer_depth,
       :turbidity,
       :water_temperature,
@@ -46,10 +52,13 @@ module BeachData
       :wave_period,
     )
 
+    # Reading is a Hashie::Mash when it is returned by soda-ruby
     def initialize(reading)
       @battery_life = reading.battery_life
       @beach_name = reading.beach_name
+      @measurement_id = reading.measurement_id
       @measurement_timestamp = set_date_time(reading.measurement_timestamp)
+      @measurement_timestamp_label = reading.measurement_timestamp_label
       @transducer_depth = reading.transducer_depth
       @turbidity = reading.turbidity
       @water_temperature = reading.water_temperature
